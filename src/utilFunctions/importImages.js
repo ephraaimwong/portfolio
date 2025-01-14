e2d79f60fc734ce9ae9e04a9ae4744b4255a7d81
ephraaimwong <73452838+ephraaimwong@users.noreply.github.com>
2025-01-14 17:48:47 -0600

    folder scraping

--- src/utilFunctions/importImages.js
+++ src/utilFunctions/importImages.js
@@ -1,4 +1,4 @@
-const images = import.meta.glob('../assets/HeadShot-Collage/*.{png,jpg,jpeg,svg}', { eager: true });
+const images = import.meta.glob('../assets/HeadShot-Collage/*.{png,jpg,jpeg,svg,webp}', { eager: true });
 
 const imagePaths = Object.values(images).map((image) => image.default);
 
